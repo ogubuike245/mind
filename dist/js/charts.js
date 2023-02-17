f44c3ab8dbd4338c2176feb9ec04f2a178d607a0
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-02-17 22:13:32 +0100

    Update charts.js

--- dist/js/charts.js
+++ dist/js/charts.js
@@ -4,8 +4,23 @@ const display = courses.map((course) => course.documents.length);
 let data = courses.map((course) => course.registeredUsers.length);
 
 const boardOne = document.getElementById("pie").getContext("2d");
-const pieBgColors = ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"];
-const pieBorderColors = ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"];
+const barBgColors = [
+  "rgba(255, 99, 132, 0.6)",
+  "rgba(54, 162, 235, 0.6)",
+  "rgba(255, 206, 86, 0.6)",
+  "rgba(75, 192, 192, 0.6)",
+  "rgba(153, 102, 255, 0.6)",
+  "rgba(255, 159, 64, 0.6)",
+];
+const barBorderColors = [
+  "rgba(255, 99, 132, 0.8)",
+  "rgba(54, 162, 235, 0.8)",
+  "rgba(255, 206, 86, 0.8)",
+  "rgba(75, 192, 192, 0.8)",
+  "rgba(153, 102, 255, 0.8)",
+  "rgba(255, 159, 64, 0.8)",
+];
+
 
 const chartOne = new Chart(boardOne, {
   type: "bar",
@@ -15,8 +30,8 @@ const chartOne = new Chart(boardOne, {
       {
         label: "DOCUMENTS UPLOADED PER COURSE",
         data: display,
-        backgroundColor: pieBgColors,
-        borderColor: pieBorderColors,
+        backgroundColor: barBgColors,
+        borderColor: barBorderColors,
         borderWidth: 3,
       },
     ],
@@ -39,22 +54,7 @@ const chartOne = new Chart(boardOne, {
 });
 
 const boardTwo = document.getElementById("bar").getContext("2d");
-const barBgColors = [
-  "rgba(255, 99, 132, 0.6)",
-  "rgba(54, 162, 235, 0.6)",
-  "rgba(255, 206, 86, 0.6)",
-  "rgba(75, 192, 192, 0.6)",
-  "rgba(153, 102, 255, 0.6)",
-  "rgba(255, 159, 64, 0.6)",
-];
-const barBorderColors = [
-  "rgba(255, 99, 132, 0.8)",
-  "rgba(54, 162, 235, 0.8)",
-  "rgba(255, 206, 86, 0.8)",
-  "rgba(75, 192, 192, 0.8)",
-  "rgba(153, 102, 255, 0.8)",
-  "rgba(255, 159, 64, 0.8)",
-];
+
 
 const chartTwo = new Chart(boardTwo, {
   type: "bar",
