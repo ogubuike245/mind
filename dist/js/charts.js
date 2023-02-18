99e4c3ef6ac12fe0a1ef284619304b96a23a479b
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-02-19 00:30:35 +0100

    modifications

--- dist/js/charts.js
+++ dist/js/charts.js
@@ -1,7 +1,14 @@
 let courses = window.courses;
 const title = courses.map((course) => course.title);
-const displayDocumentsUploadedPerCourseLength= courses.map((course) => course.documents.length);
-const displayRegisteredUsersLength    = courses.map((course) => course.registeredUsers.length);
+const displayDocumentsUploadedPerCourseLength = courses.map(
+  (course) => course.documents.length
+);
+const displayRegisteredUsersLength = courses.map(
+  (course) => course.registeredUsers.length
+);
+const displayDownloadedDocumentLength = courses.map(
+  (course) => course.registeredUsers.length
+);
 
 const barBgColors = [
   "rgba(255, 99, 132, 0.6)",
@@ -19,8 +26,6 @@ const barBorderColors = [
   "rgba(153, 102, 255, 0.8)",
   "rgba(255, 159, 64, 0.8)",
 ];
-
-
 
 const boardOne = document.getElementById("pie").getContext("2d");
 
@@ -57,7 +62,6 @@ const chartOne = new Chart(boardOne, {
 
 const boardTwo = document.getElementById("bar").getContext("2d");
 
-
 const chartTwo = new Chart(boardTwo, {
   type: "bar",
   data: {
@@ -66,6 +70,38 @@ const chartTwo = new Chart(boardTwo, {
       {
         label: "REGISTERED USERS PER COURSE",
         data: displayRegisteredUsersLength,
+        backgroundColor: barBgColors,
+        borderColor: barBorderColors,
+        borderWidth: 3,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            callback: function (value) {
+              if (Number.isInteger(value)) {
+                return value;
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
+});
+const boardThree = document.getElementById("line").getContext("2d");
+
+const chartThree = new Chart(boardThree, {
+  type: "line",
+  data: {
+    labels: title,
+    datasets: [
+      {
+        label: "DOWNLOADED DOCUMENTS  PER COURSE",
+        data: displayDownloadedDocumentLength,
         backgroundColor: barBgColors,
         borderColor: barBorderColors,
         borderWidth: 3,
