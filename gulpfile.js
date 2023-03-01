f28315d170aa67c32922e570fd65ce19997b91b7
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-03-01 15:10:00 +0100

    gulpfile addition

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
-function buildStyles() {
-  return src('dist/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(dest('dist/css'))
+function compose() {
+  return src("dist/scss/**/*.scss").pipe(sass()).pipe(dest("dist/css"));
 }
 
-function watchTask() {
-  watch(['dist/scss/**/*.scss'], buildStyles)
+function track() {
+  watch(["dist/scss/**/*.scss"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(compose, track);
